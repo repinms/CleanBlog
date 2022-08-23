b40f2c53ba6ac72a11dd2b5aba413d474b5f9db1
repinms <110122920+repinms@users.noreply.github.com>
2022-08-23 16:54:38 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <title>CleanBlog</title>
+</head>
+<body>
+    <div class="wrapper">
+        <header>
+            <a id="logo" href="/">CleanBlog</a>
+            <nav>
+                <a href="/">Главная</a>
+                <a href="/">Все статьи</a>
+                <a href="/">Тэги</a>
+            </nav>
+            <a id="login" href="/">Войти</a>
+        </header>
+        <main>
+            <div class="banners">
+                <div class="title">Новинки</div>
+                <div class="banner_unit">
+                    <img src="content/images/yandex.jpg" alt="">
+                    <div>Статья про Яндекс</div>
+                </div>
+                <div class="banner_unit">
+                    <img src="content/images/yandex.jpg" alt="">
+                    <div>Статья про Яндекс</div>
+                </div>
+            </div>
+
+            <section>
+                <div class="card">
+                    <img src="content/images/yandex.jpg" alt="">
+                    <span class="name">Статья про Яндекс</span>
+                    <span class="preview">Это предпросмотр статьи...</span>
+                </div>
+            </section>
+        </main>
+        <footer>
+            Сайт на чистом PHP
+        </footer>
+    </div>
+</body>
+</html>
