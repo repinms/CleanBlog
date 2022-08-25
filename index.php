836c1dd33bdb42f1a3c0330126d0eba8fd6e94e0
repinms <grey.vorobey.23@gmail.com>
2022-08-25 21:07:58 +0400

    Ниче не сделал

--- index.php
+++ index.php
@@ -25,32 +25,28 @@
         </header>
 
         <main>
-            <div class="row">
-                <div class="col">
-                    <div class="card" style="height:300px; background-image: url(content/images/yandex.jpg); background-size: cover;">
-                        <div class="d-flex card-body align-items-center justify-content-end flex-column">
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <small class="text-muted">9 mins</small>
+            <div class="row d-flex justify-content-around">
+                <div class="col-5">
+                    <a class="link-href" href="/">
+                        <div class="card" style="height:300px; background-image: url(content/images/yandex.jpg); background-size: cover;">
+                            <div class="d-flex card-body align-items-center justify-content-end flex-column">
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <small class="text-muted">9 mins</small>
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
                 
             </div>
         </main>
 
-        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 mt-auto border-top">
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
             <div class="col-md-4 d-flex align-items-center">
                 <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
                     <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
                 </a>
                 <span class="mb-3 mb-md-0 text-muted">© 2022 Company, Inc</span>
             </div>
-
-            <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-                <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
-                <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
-                <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
-            </ul>
         </footer>
     </div>
 </body>
