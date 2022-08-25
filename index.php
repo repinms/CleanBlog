92eecaaf1ebf43e46c1294a0f9874358c622acb2
repinms <grey.vorobey.23@gmail.com>
2022-08-25 17:04:57 +0400

    Прогресс

--- index.php
+++ index.php
@@ -26,6 +26,14 @@
 
         <main>
             <div class="row">
+                <div class="col">
+                    <div class="card" style="height:300px; background-image: url(content/images/yandex.jpg); background-size: cover;">
+                        <div class="d-flex card-body align-items-center justify-content-end flex-column">
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <small class="text-muted">9 mins</small>
+                        </div>
+                    </div>
+                </div>
                 
             </div>
         </main>
